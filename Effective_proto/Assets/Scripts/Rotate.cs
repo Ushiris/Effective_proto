30d0523a2677d2b982a057fc830f269c9a2c2db7
ushiris_home <ushiriscco@gmail.com>
2020-04-16 16:23:57 +0900

    add textmesh and previwe scene

---
+++ Effective_proto/Assets/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 axle = Vector3.up;
+    [SerializeField]
+    float speed = 0.6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(axle, speed);
+    }
+}
