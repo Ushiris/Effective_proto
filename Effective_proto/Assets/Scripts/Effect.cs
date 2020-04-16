9646fcd37c43d7d02055773175b178d9b2cc2b3c
ushiris_home <ushiriscco@gmail.com>
2020-04-16 18:39:50 +0900

    add effect make script

---
+++ Effective_proto/Assets/Scripts/Effect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Effect : MonoBehaviour
+{
+    [SerializeField]
+    string effect_name = "undifined";
+    [SerializeField]
+    GameObject effect_bace;
+    [SerializeField]
+    bool StartSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(StartSpawn)
+        {
+            Spawn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject Spawn()
+    {
+        GameObject instance = Instantiate<GameObject>(effect_bace);
+        instance.GetComponent<TextMeshPro>().text = effect_name;
+        return instance;
+    }
+}
