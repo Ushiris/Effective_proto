3f5db3006d444e05434e3fd340693e6c1a76cdf0
ushiris_home <ushiriscco@gmail.com>
2020-04-16 21:52:51 +0900

    add effect box

---
+++ Effective_proto/Assets/Scripts/EffectBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectBox : MonoBehaviour
+{
+    List<string> box;
+
+    public void AddEffect(string effect_name)
+    {
+        box.Add(effect_name);
+    }
+
+    public bool IsHold(string effect_name)
+    {
+        return box.Contains(effect_name);
+    }
+
+    public bool IsHoldAll(List<string> effect_names)
+    {
+        foreach (var item in effect_names)
+        {
+            if(!box.Contains(item))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
