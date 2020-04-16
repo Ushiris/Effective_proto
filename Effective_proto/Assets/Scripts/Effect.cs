0addf201c5bf83b1b7b5f3322c0b606c6a2d78bb
ushiris_home <ushiriscco@gmail.com>
2020-04-16 22:08:23 +0900

    add pickup effect script

--- Effective_proto/Assets/Scripts/Effect.cs
+++ Effective_proto/Assets/Scripts/Effect.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Effect : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag=="Player")
+        {
+            EffectBox box = other.GetComponent<EffectBox>();
+            if (box == null)
+            {
+                box = other.gameObject.AddComponent<EffectBox>();
+            }
+
+            box.AddEffect(gameObject.GetComponent<TextMeshPro>().text);
+        }
     }
 }
